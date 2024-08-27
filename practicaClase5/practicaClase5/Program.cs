875d2ca08f8b2c01903263b079aa6461fe6018d4
LuisMi1707 <TuCorreo@example.com>
2024-08-27 17:28:54 -0600

    Explicacion de "Program" de "practicaClase5"

--- practicaClase5/practicaClase5/Program.cs
+++ practicaClase5/practicaClase5/Program.cs
@@ -6,17 +6,24 @@ using System.Windows.Forms;
 
 namespace practicaClase5
 {
+    // Clase principal de la aplicación
     internal static class Program
     {
         /// <summary>
         /// Punto de entrada principal para la aplicación.
         /// </summary>
-        [STAThread]
+        [STAThread] // Indica que el modelo de subprocesos del programa es Single-Threaded Apartment (STA), necesario para Windows Forms.
         static void Main()
         {
+            // Habilita estilos visuales para la aplicación (aplicación de estilos modernos de Windows)
             Application.EnableVisualStyles();
+
+            // Establece que se use el motor de renderizado de texto compatible (opción predeterminada para Windows Forms)
             Application.SetCompatibleTextRenderingDefault(false);
+
+            // Inicia la ejecución de la aplicación con el formulario principal (Form1)
             Application.Run(new Form1());
         }
     }
 }
+
